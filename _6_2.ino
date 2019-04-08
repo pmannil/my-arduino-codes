3932348fe26a8e8fd7bfb311d10593769e267622
pmannil <45764461+pmannil@users.noreply.github.com>
2019-04-08 17:02:51 +0530

    Add files via upload

---
+++ _6_2.ino
@@ -0,0 +1,40 @@
+int led=5;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while(Serial.available()==0);
+  String n= Serial.readStringUntil('\n');
+  Serial.println(n); 
+  int level;
+
+  if(n=="one")
+  {
+    level=255/5;
+  }
+  else if(n=="two")
+  {
+    level=(255/5)*2;
+  }
+  else if(n=="three")
+  {
+    level=(255/5)*3;
+  }
+  else if(n=="four")
+  {
+    level=(255/5)*4;
+  }
+  else 
+  {
+    level=255;
+  }
+  Serial.println(level);
+  analogWrite(led,level);    
+  
+}
+  
